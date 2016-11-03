a70070595be24e9636a45dedc5819a3f5cf5b532
Henry Steinitz <Hjsteinitz@gmail.com>
2016-11-03 15:15:20 -0400

    remove warning

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -26,5 +26,17 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-maps',
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
 };
